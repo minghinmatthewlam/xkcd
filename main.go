281f3e0a8c5658dd9eb28a97858266e9e21d2acc
Matthew Lam <matthewlam96@hotmail.com>
2022-08-09 23:28:18 -0700

    draft xkcd with bugs

--- main.go
+++ main.go
@@ -5,8 +5,10 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 )
 
@@ -24,9 +26,56 @@ type Result struct {
 	Day        string `json:"day"`
 }
 
+type Job struct {
+	number int
+}
+
 const (
 	URL string = "https://xkcd.com"
 )
+
+var jobs = make(chan Job, 100)
+var results = make(chan Result, 100)
+var resultCollection []Result
+
+func getResults(done chan bool) {
+	for result := range results {
+		if result.Num != 0 {
+			fmt.Println("Retrieving issue", result.Num)
+			resultCollection = append(resultCollection, result)
+		}
+	}
+	done <- true
+}
+
+func worker(wg *sync.WaitGroup) {
+	for job := range jobs {
+		result, err := fetch(job.number)
+		if err != nil {
+			log.Println("error in fetching: ", err)
+		}
+		results <- *result
+		wg.Done()
+	}
+}
+
+func createWorkerPool(numWorkers int) {
+	var wg sync.WaitGroup
+	for i := 0; i < numWorkers; i++ {
+		wg.Add(1)
+		go worker(&wg)
+	}
+
+	wg.Wait()
+	close(results)
+}
+
+func allocateJobs(numJobs int) {
+	for i := 0; i < numJobs; i++ {
+		jobs <- Job{i + 1}
+	}
+	close(jobs)
+}
 
 func fetch(n int) (*Result, error) {
 	client := &http.Client{
@@ -45,25 +94,54 @@ func fetch(n int) (*Result, error) {
 		return nil, fmt.Errorf("http error doing request: %e", err)
 	}
 
-	var data *Result
+	var data Result
+
+	// error from web service, empty struct to avoid disruption of process
 	if resp.StatusCode != http.StatusOK {
-		data = new(Result)
+		data = Result{}
 	} else {
-		if err := json.NewDecoder(resp.Body).Decode(data); err != nil {
-			return nil, fmt.Errorf("json decode error: %e", err)
+		if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
+			return nil, fmt.Errorf("json err: %v", err)
 		}
 	}
 
-	defer resp.Body.Close()
-	return data, nil
+	resp.Body.Close()
+
+	return &data, nil
+}
+
+func writeToFile(data []byte) error {
+	f, err := os.Create("xkcd.json")
+	if err != nil {
+		return err
+	}
+
+	_, err = f.Write(data)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func main() {
-	n := 200
-	result, err := fetch(n)
+	numJobs := 3000
+	go allocateJobs(numJobs)
+
+	done := make(chan bool)
+	go getResults(done)
+
+	numWorkers := 100
+	createWorkerPool(numWorkers)
+
+	<-done
+
+	data, err := json.MarshalIndent(resultCollection, "", "    ")
 	if err != nil {
-		log.Fatalln(err)
+		log.Fatalf("error in marshalling result collection: %e", err)
 	}
 
-	fmt.Println(result.Title)
+	err = writeToFile(data)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
